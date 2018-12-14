b42432cd776bbcf2de8e4f9b9d9be916cb811fd0
Teema Luksameset <teema.ma.luk@kkumail.com>
2018-12-14 09:15:20 +0700

    about to finish

--- api/controller/chompusudsuay.js
+++ api/controller/chompusudsuay.js
@@ -36,42 +36,36 @@ exports.send = async function (req, res) {
 
     var input_Barcode = input.UID;
     var root = firebase.database();
-    var user_email = 'invalid';
-
-    /*
-        todo: *done
-            check barcode is equal to active barcodes or not
-            if it isn't, drop.
-            if it is got the email of barcode's user into a variable and got ref of active barcode
-    */
 
     await root.ref('API_DATA').orderByChild('barcode_num').equalTo(input_Barcode)
-        .once('child_added').then(function (snapshot) {
+        .on('child_added', async function (snapshot) {
             // console.log('Found!');
-            // console.log(snapshot.child('email').val());
-            user_email = snapshot.child('email').val();
+            var user_email;
+            await root.ref('API_DATA/' + snapshot.key).once('value').then(function (snaps){ 
+                user_email = snapshot.val().email;
+                console.log(user_email);
+            });
+            console.log(user_email);
+            console.log("Pushing Data to firebase...");
+            var slip_ID = root.ref('SLIP_DATA').push(slip).path.pieces_[1]; // push slip data to firebase and got slip id
+            await root.ref('USER_DATA').orderByChild('email').equalTo(user_email)
+            .on('child_added', async function (snap) {
+                console.log(snap.key);
+                await root.ref('USER_DATA/' + snap.key + '/slip').push({
+                    date_time: input.date_time,
+                    slip_id: slip_ID,
+                    total_price: input.total_price
+                });
+                console.log("Pushed slip " + slip_ID + " for user barcode " + input_Barcode);
+            });
+
         });
 
-    /* ============================================================== */
+    // admin.database().ref('USER_DATA');
+    // ref.push(obj).path.pieces_[1]
 
-    if (user_email == 'invalid') {
-        console.log('Slip droped');
-    }
-    else {
-        console.log("Pushing Data to firebase...");
-        var slip_ID = root.ref('SLIP_DATA').push(slip).path.pieces_[1]; // push slip data to firebase and got slip id
-        console.log("Pushed slip " + slip_ID + " for user barcode " + input_Barcode);
+    await res.json({
+        status: "sent successful!"
+    });
 
-        /*
-            todo: *done
-                use email to add slip to that user (slip_id, date_time, total_price)
-        */
-
-        // admin.database().ref('USER_DATA');
-        // ref.push(obj).path.pieces_[1]
-
-        await res.json({
-            status: "sent successful!"
-        });
-    }
 }
