61e03e9315a4e1684425c53b457400e0516640fb
Teema Luksameset <teema.ma.luk@kkumail.com>
2019-01-21 21:18:24 +0700

    Change Json Point

--- api/controller/point.js
+++ api/controller/point.js
@@ -4,6 +4,8 @@ var User = require('../models/userData');
 
 exports.get = function (req, res) {
     User.findById(req.query.uid, function (err, docs) {
-        res.json(docs.point);
+        res.json({
+            points: docs.point
+        });
     });
 }
