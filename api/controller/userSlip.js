e79d55992b4f8a3f441475dfac5ed712f453264e
Teema Luksameset <teema.ma.luk@kkumail.com>
2019-01-22 22:06:07 +0700

    Update all slip response object

--- api/controller/userSlip.js
+++ api/controller/userSlip.js
@@ -1,11 +1,12 @@
 'use stricts';
 
-var firebase = require('./FirebaseAdmin');
 var User = require('../models/userData');
 
 exports.getAll = function(req, res) {
     console.log('get all uid ' + req.query.uid + ' slip');
     User.findOne({_id: req.query.uid}, function(err, doc) {
-        res.json(doc.slip);
+        res.json({
+            slips: doc.slip
+        });
     });
 }
